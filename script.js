4e824dfee21234e96bf253010a556c84e47c6f82
chersoftdz <aminach4678@gmail.com>
2026-08-07 09:53:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -875,7 +875,7 @@ function showSituations() {
 
 // Function to show calculator
 function showCalculator() {
-    window.location.href = 'calculatrice.html';
+    window.location.href = 'calculatrice%20scientifique.html';
 }
 
 // Graphing functions removed - handled inline in index.html
